a9dd2051996d3d145330651a9cc713c8de48e268
anshulchikhale30p <anshulchikhale30p@gmail.com>
2026-02-19 18:02:42 +0530

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use gilrs::Gilrs;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("🎮 Rein Controller Prototype Started");
+
+    let mut gilrs = Gilrs::new().unwrap();
+
+    loop {
+        while let Some(event) = gilrs.next_event() {
+            println!("Gamepad: {} - Event: {:?}", event.id, event.event);
+        }
+
+        thread::sleep(Duration::from_millis(10));
+    }
+}
