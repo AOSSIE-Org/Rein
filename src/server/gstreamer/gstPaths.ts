fe8e346a4d614fcd052c6c1bbdbdfb3de7299e0c
pinjinx <rohithkpradeep@gmail.com>
2026-07-20 14:28:17 +0530

    [Gstreamer Bundle] CI Fixes

--- src/server/gstreamer/gstPaths.ts
+++ src/server/gstreamer/gstPaths.ts
@@ -76,14 +76,12 @@ function bundledPaths(): GstPaths {
 	if (os.platform() === "win32") {
 		const existingPath = process.env.PATH ?? ""
 		env.PATH = `${binDir};${existingPath}`
-	}
-	else if (os.platform() === "linux") {
+	} else if (os.platform() === "linux") {
 		const existingLdPath = process.env.LD_LIBRARY_PATH ?? ""
 		env.LD_LIBRARY_PATH = existingLdPath
 			? `${binDir}:${existingLdPath}`
 			: binDir
-	}
-	else if (os.platform() === "darwin") {
+	} else if (os.platform() === "darwin") {
 		const existingDyldPath = process.env.DYLD_LIBRARY_PATH ?? ""
 		const libDir = path.join(BUNDLED_GSTREAMER_ROOT, "lib")
 		env.DYLD_LIBRARY_PATH = existingDyldPath
